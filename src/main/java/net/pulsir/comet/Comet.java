f90c34d36d163c4a32bb0cda4f60ead7fc2bb257
KacperM <vkacperpl@gmail.com>
2024-05-18 20:06:46 +0200

    project base

---
+++ src/main/java/net/pulsir/comet/Comet.java
@@ -0,0 +1,16 @@
+package net.pulsir.comet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Comet extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
